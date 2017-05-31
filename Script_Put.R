466a761c402222db8de4cb04d5b8f69cee387922
duhi23 <diego.21.phs@hotmail.com>
2017-06-01 01:01:10 +0200

    Explicit Finite Difference Method

--- Script_Put.R
+++ Script_Put.R
@@ -12,7 +12,8 @@
 # M: numero de incrementos en el subyacente
 # N: numero de incrementos en el tiempo
 
-put <- function(S,X,r,sigma,Smax,M,N){
+# Metodo Implicito
+put_imp <- function(S,X,r,sigma,Smax,M,N){
       dt <- 1
       ds <- Smax/M
       # Estimacion parametros
@@ -20,9 +21,9 @@ put <- function(S,X,r,sigma,Smax,M,N){
       b <- numeric(M+1)
       c <- numeric(M+1)
       for(j in 2:M){
-            a[j] <- 0.5*r*j*dt -0.5*sigma^2*j^2*dt
-            b[j] <- 1 + sigma^2*j^2*dt + r*dt
-            c[j] <- -0.5*r*j*dt -0.5*sigma^2*j^2*dt
+            a[j] <- 0.5*r*(j-1)*dt -0.5*sigma^2*(j-1)^2*dt
+            b[j] <- 1 + sigma^2*(j-1)^2*dt + r*dt
+            c[j] <- -0.5*r*(j-1)*dt -0.5*sigma^2*(j-1)^2*dt
       }
       # Condiciones
       f <- matrix(0, ncol=N+1, nrow = M+1)
@@ -50,7 +51,44 @@ put <- function(S,X,r,sigma,Smax,M,N){
             vec <- f[2:M,i+1] - c(a[2]*f[1,i], rep(0,M-3) , c[M]*f[M+1,i])
             f[2:M,i] <- apply(solve(XM)%*%diag(vec), MARGIN = 1, sum)
       }
-      return(list(Malla=f, ValorPut=f[(X/ds)+1,1]))
+      return(list(Malla=round(f,4), ValorPut=f[(X/ds)+1,1]))
 }
 
-put(50,50,0.02,0.2,100,10,4)
+put_imp(50,50,0.02,0.2,100,10,4)
+
+
+# Metodo explicito
+put_exp <- function(S,X,r,sigma,Smax,M,N){
+      dt <- 1
+      ds <- Smax/M
+      # Estimacion parametros
+      a <- numeric(M+1)
+      b <- numeric(M+1)
+      c <- numeric(M+1)
+      for(j in 1:(M+1)){
+            a[j] <- (-0.5*r*(j-1)*dt + 0.5*sigma^2*(j-1)^2*dt)/(1+r*dt)
+            b[j] <- (1 - sigma^2*(j-1)^2*dt)/(1+r*dt)
+            c[j] <- (0.5*r*(j-1)*dt + 0.5*sigma^2*(j-1)^2*dt)/(1+r*dt)
+      }
+      f <- matrix(0, ncol=N+1, nrow = M+1)
+      # Valor intrinseco
+      for(j in 1:(M+1)){
+            f[j,N+1] <- max(X-(j-1)*ds,0)
+      }
+      # Si el subyacente es cero
+      for(i in 1:(N+1)){
+            f[1,i] <- X
+      }
+      # Si el subyacente alcanza el maximo
+      for(i in 1:(N+1)){
+            f[M+1,i] <- 0
+      }
+      for(i in N:1){
+            for(j in 2:M){
+                  f[j,i] <- a[j]*f[j-1,i+1] + b[j]*f[j,i+1] + c[j]*f[j+1,i+1]
+            }
+      }
+      return(list(Malla=round(f,4), ValorPut=f[(X/ds)+1,1]))
+}
+
+put_exp(50,50,0.02,0.2,100,10,4)
